7d4687f8af2c039a80975f8127eeaad04767d527
Volodymyr <cherk.v8@gmail.com>
2018-02-27 21:39:54 +0000

    some-remark

--- js/index.js
+++ js/index.js
@@ -5,59 +5,61 @@ const a = parseFloat(prompt('Enter a '));
 const b = parseFloat(prompt('Enter b '));
 const c = parseFloat(prompt('Enter c '));
 
-const answer = solveQuadraticEquation(x1, x2);
-document.write(answer);
+solveQuadraticEquation();
 
-function solveQuadraticEquation(x1, x2) {
-    const d = discr(a, b, c);
+function solveQuadraticEquation() {
+    const d = b * b - (4 * a * c);
     if (d < 0) {
-        alert('There are no valid solutions');
+        alert('There are no valid solutions.');
     }
     else if (d === 0) {
-        x1 = x2 = (-b) / (2 * a);
+        x1 = x2 = (-b) / 2 * a;
+        document.write('x<sub>1</sub> = x<sub>2</sub> = ' + x2 + '.');
     }
     else {
-        x1 = (-b + Math.sqrt(d)) / (2 * a);
-        x2 = (-b - Math.sqrt(d)) / (2 * a);
+        x1 = (-b + Math.sqrt(d)) / 2 * a;
+        x2 = (-b - Math.sqrt(d)) / 2 * a;
+        document.write('x<sub>1</sub> = ' + x1 + ';');
+        document.write('<br>');
+        document.write('x<sub>2</sub> = ' + x2 + '.');
     }
-    return [x1, x2];
-}
-
-function discr(a, b, c) {
-    let d = b * b - 4 * a * c;
-    return d;
 }
 
 
 
-
-
-// let x1 = 0;
-// let x2 = 0;
 
 // const a = parseFloat(prompt('Enter a '));
 // const b = parseFloat(prompt('Enter b '));
 // const c = parseFloat(prompt('Enter c '));
 
-// solveQuadraticEquation();
+// const answer = solveQuadraticEquation();
+// document.write(answer);
 
-// function solveQuadraticEquation() {
-//     const d = b * b - (4 * a * c);
+// function solveQuadraticEquation(x1, x2) {
+//     const d = discr(a, b, c);
 //     if (d < 0) {
-//         alert('There are no valid solutions.');
+//         alert('There are no valid solutions');
 //     }
 //     else if (d === 0) {
-//         x1 = x2 = (-b) / 2 * a;
-//         document.write('x<sub>1</sub> = x<sub>2</sub> = ' + x2 + '.');
+//         x1 = x2 = (-b) / (2 * a);
 //     }
 //     else {
-//         x1 = (-b + Math.sqrt(d)) / 2 * a;
-//         x2 = (-b - Math.sqrt(d)) / 2 * a;
-//         document.write('x<sub>1</sub> = ' + x1 + ';');
-//         document.write('<br>');
-//         document.write('x<sub>2</sub> = ' + x2 + '.');
+//         x1 = (-b + Math.sqrt(d)) / (2 * a);
+//         x2 = (-b - Math.sqrt(d)) / (2 * a);
 //     }
+//     return [x1, x2];
 // }
+
+// function discr(a, b, c) {
+//     let d = b * b - 4 * a * c;
+//     return d;
+// }
+
+
+
+
+
+
 
 
 
