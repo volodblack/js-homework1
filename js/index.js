6a42181b7e56f4da2dc6ed4f4558c5beffac43f7
Volodymyr <cherk.v8@gmail.com>
2018-03-01 09:54:05 +0000

    Finish

--- js/index.js
+++ js/index.js
@@ -1,83 +1,28 @@
-
 const a = parseFloat(prompt('Enter a '));
 const b = parseFloat(prompt('Enter b '));
 const c = parseFloat(prompt('Enter c '));
 
-const answer = solveQuadraticEquation();
+const answer = solveQuadraticEquation(a, b);
 document.write(answer);
 
-function solveQuadraticEquation(x1, x2) {
+function solveQuadraticEquation(a, b) {
     const d = discr(a, b, c);
     if (d < 0) {
         alert('There are no valid solutions');
     }
     else if (d === 0) {
-        x1 = x2 = (-b) / (2 * a);
+       const x1 = (-b) / (2 * a);
+       const x2 = (-b) / (2 * a);
+       return `x1 = ${x1} , and x2 = ${x2}`;
     }
     else {
-        x1 = (-b + Math.sqrt(d)) / (2 * a);
-        x2 = (-b - Math.sqrt(d)) / (2 * a);
+       const x1 = (-b + Math.sqrt(d)) / (2 * a);
+       const x2 = (-b - Math.sqrt(d)) / (2 * a);
+       return `x1 = ${x1} , and x2 = ${x2}`;
     }
-    return [x1, x2];
 }
 
 function discr(a, b, c) {
-    let d = b * b - 4 * a * c;
+    const d = b * b - 4 * a * c;
     return d;
 }
-
-
-
-// let x1 = 0;
-// let x2 = 0;
-
-// const a = parseFloat(prompt('Enter a '));
-// const b = parseFloat(prompt('Enter b '));
-// const c = parseFloat(prompt('Enter c '));
-
-// solveQuadraticEquation();
-
-// function solveQuadraticEquation() {
-//     const d = b * b - (4 * a * c);
-//     if (d < 0) {
-//         alert('There are no valid solutions.');
-//     }
-//     else if (d === 0) {
-//         x1 = x2 = (-b) / 2 * a;
-//         document.write('x<sub>1</sub> = x<sub>2</sub> = ' + x2 + '.');
-//     }
-//     else {
-//         x1 = (-b + Math.sqrt(d)) / 2 * a;
-//         x2 = (-b - Math.sqrt(d)) / 2 * a;
-//         document.write('x<sub>1</sub> = ' + x1 + ';');
-//         document.write('<br>');
-//         document.write('x<sub>2</sub> = ' + x2 + '.');
-//     }
-// }
-
-
-
-
-// let x1 = 0;
-// let x2 = 0;
-
-// const a = parseFloat(prompt('Enter a '));
-// const b = parseFloat(prompt('Enter b '));
-// const c = parseFloat(prompt('Enter c '));
-
-// const d = b * b - (4 * a * c);
-
-// if (d < 0) {
-//     alert('There are no valid solutions');
-// }
-// else if (d === 0) {
-//     x1 = x2 = (-b) / 2 * a;
-//     document.write('x1, x2 = ' + x2);
-// }
-// else {
-//     x1 = (-b + Math.sqrt(d)) / 2 * a;
-//     x2 = (-b - Math.sqrt(d)) / 2 * a;
-//     document.write('x1 = ' + x1);
-//     document.write('<br>');
-//     document.write('x2 = ' + x2);
-// }
